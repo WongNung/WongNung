bab413545eea558340b325817d2838e019613926
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2022-10-13 14:29:38 +0700

    Add docstring for models.

--- wongnung/models.py
+++ wongnung/models.py
@@ -6,6 +6,24 @@ import tmdbsimple as tmdb
 
 
 class Film(models.Model):
+    """
+    Model for Film with film ID
+
+    :param filmId: The ID of a specific film.
+    :type filmId: int
+    :param title: The title of a film.
+    :type title: str
+    :param year_released: The release year of the film.
+    :type year_released: int
+    :param director: The name of the director(s) of the film.
+    :type director: str
+    :param genres: The genres of the film.
+    :type genres: str
+    :param summary: A summary of the film.
+    :type summary: str
+    :param stars: The stars(actors and actresses) of the film
+    :type stars: str
+    """
     filmId = models.IntegerField(primary_key=True)  # Required
     title = models.CharField(max_length=256)  # Required
     year_released = models.IntegerField()  # Required
@@ -15,28 +33,64 @@ class Film(models.Model):
     stars = models.CharField(max_length=1000, blank=True)  # names of
 
     def get_director(self):
+        """Get a list contain the name of the director(s) of the film
+
+        :return: A list containing the string representing the name of the director(s) of the film
+        :rtype: list
+        """
         return [director.strip() for director in
                 self.director.split(",")]
 
     def get_genres(self):
+        """Get a list contain the genres of the film
+
+        :return: A list containing the string representing the genres of the film
+        :rtype: list
+        """
         return [genre.strip() for genre in
                 self.genres.split(",")]
 
     def get_stars(self):
+        """Get a list contain the genres of the film
+
+        :return: A list containing the string representing name of the stars of the film
+        :rtype: list
+        """
         return [star.strip() for star in
                 self.stars.split(",")]
 
     def set_director(self, directors: List[str]):
+        """Get a list containing the name of the director(s) of the film and set it to
+        be a director attribute
+
+        :param directors: A list containing the string representing the name of the director(s)
+        of the film
+        """
         self.director = ", ".join(directors)
 
     def set_genres(self, genres: List[str]):
+        """Get a list containing the genres and set it to be a genres attribute
+
+        :param genres: A list containing the string representing the genres of the film
+        """
         self.genres = ", ".join(genres)
 
     def set_stars(self, stars: List[str]):
+        """Get a list containing the stars of the film and set it to be a stars attribute
+
+        :param stars: A list containing the string representing the stars of the film
+        """
         self.stars = ", ".join(stars)
 
     @classmethod
     def get_film(cls, film_id):
+        """Create and return Film object
+
+        :param film_id: The ID of a specific film.
+        :type film_id: int
+        :return: film object
+        :rtype: Film
+        """
         try:
             film = cls.objects.get(pk=film_id)
             return film
@@ -66,6 +120,16 @@ class Film(models.Model):
 
 
 class Review(models.Model):
+    """
+    Model for Review with Film object and publishing date
+
+    :param film: A Film object
+    :type film: Film
+    :param pub_date: A date object representing the published date of the review
+    :type pub_date: Datetime
+    :param content: A string representation of the content of this Review
+    :type content: str
+    """
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(default=datetime.now())
     content = models.CharField(max_length=1000)
