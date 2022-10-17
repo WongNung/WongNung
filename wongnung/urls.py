ffba0ba01cc98990e5091056a01671f9d0bf74eb
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-10-17 20:04:58 +0700

    Creates a working search bar in navbar

--- wongnung/urls.py
+++ wongnung/urls.py
@@ -1,7 +1,13 @@
-from .views import show_film_component
+from .views import *
 from django.urls import path
 
 app_name = "wongnung"
 urlpatterns = [
-    path('show_film_component/<str:filmid>', show_film_component, name='film-component')
+    path("test", test_page, name="test"),
+    path(
+        "show_film_component/<str:filmid>",
+        show_film_component,
+        name="film-component",
+    ),
+    path("search", search, name="search"),
 ]
--- wongnung/views.py
+++ wongnung/views.py
@@ -1,10 +1,61 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 from .models import Film
+import tmdbsimple as tmdb
+
+__all__ = ["show_film_component", "test_page", "search"]
+
+
+def test_page(request):
+    return render(request, "wongnung/test_page.html")
 
 
 def show_film_component(request, filmid):
     film = Film.get_film(film_id=filmid)
-    context = {
-        'film': film
-    }
-    return render(request, 'wongnung/film_component.html', context)
+    context = {"film": film}
+    return render(request, "wongnung/film_component.html", context)
+
+
+def search(request: HttpRequest):
+    query = request.GET.get("query")
+
+    result_cls = [
+        "m-1",
+        "text-black",
+        "hover:text-component-red",
+    ]
+
+    results_html = ""
+    if len(query) >= 3:
+        search = tmdb.Search()
+        res = search.movie(query=query)["results"]
+        if res:
+            if len(res) > 5:
+                res = res[:5]
+            for film in res:
+                year = film["release_date"].split("-")[0]
+                results_html += f"""<span class="{" ".join(result_cls)}">
+                <a href="/show_film_component/{film["id"]}"
+                class="hover:underline">{film["title"]} ({year})</a>
+                </span>"""
+
+    container_cls = [
+        "mx-6",
+        "my-4",
+        "px-5",
+        "py-4",
+        "absolute",
+        "overflow-x-hidden",
+        "max-w-3xl",
+        "flex",
+        "flex-col",
+        "bg-white",
+        "rounded-md",
+        "drop-shadow-lg",
+        f"{'' if len(query) >= 3 and results_html else 'hidden'}",
+    ]
+
+    html = f'<div class="{" ".join(container_cls)}" id="search-results">'
+    html += results_html
+    html += f"</div>"
+    return HttpResponse(html)
