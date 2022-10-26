9ed3e46f45305030f02759db9a2ef5e3becae35b
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2022-10-26 18:59:37 +0700

    Modify content attribute in Report model.

--- wongnung/models.py
+++ wongnung/models.py
@@ -222,8 +222,7 @@ class Report(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     report_date = models.DateTimeField(default=timezone.now)
-    content = models.CharField(
-        max_length=1000, blank=False, null=False, default=None)
+    content = models.CharField(max_length=1000)
 
     def __str__(self):
         return f"{self.user} reported {self.review} at {self.report_date}."
