5cb39d5a03a510bf6ce19685b8e4c147ffa7ee87
Supakrit65 <supakrit.tan65@hotmail.com>
2022-11-04 20:07:10 +0700

    implement delete bookmark functionality

--- wongnung/models/bookmark.py
+++ wongnung/models/bookmark.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
-
+from typing import Union
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.db import models
 
+from .review import Review
+from .film import Film
+from .fandom import Fandom
 
-def get_bookmark_item_set(ct: ContentType, owner: User):
+
+def get_bookmark_item_set(ct: ContentType, owner: User) -> \
+        list[Union[Film, Review, Fandom]]:
     """Get bookmarks for certain user with specified type."""
     return [bookmark.content_object for
             bookmark in Bookmark.objects.filter(content_type=ct, owner=owner)]
+
+
+def delete_bookmark(ct: ContentType, owner: User, obj_id: int):
+    """Delete a specified bookmark."""
+    Bookmark.objects.filter(content_type=ct, owner=owner, object_id=obj_id).delete()
 
 
 class Bookmark(models.Model):
