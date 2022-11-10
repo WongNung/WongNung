e02c4f0eb1cf49b168ab98d630360b4574480b2a
Supakrit Aphonmaeklong <92836314+Supakrit65@users.noreply.github.com>
2022-11-10 14:26:04 +0700

    remove unuse import
    Co-authored-by: GToidZ <39559733+GToidZ@users.noreply.github.com>

--- pdtx/urls.py
+++ pdtx/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
