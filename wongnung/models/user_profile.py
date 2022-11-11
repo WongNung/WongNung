893f25ced69def30b4885162d12a4aae99d18960
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-11-11 11:43:46 +0700

    Formats the user profile model

--- wongnung/models/user_profile.py
+++ wongnung/models/user_profile.py
@@ -5,12 +5,10 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        primary_key=True
+        User, on_delete=models.CASCADE, primary_key=True
     )
     display_name = models.CharField(max_length=32, blank=True)
-    color = models.CharField(max_length=6, default="D9D9D9")
+    _color = models.CharField(max_length=6, default="D9D9D9")
 
     def __str__(self):
-        return f""
+        return f"User Profile of {self.user.username}"
