2cadb0df8f125298969a58d8e9cf73ae3062032b
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-11-12 16:47:37 +0700

    Removes unneeded statement

--- wongnung/apps.py
+++ wongnung/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class WongnungConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "wongnung"
-
-    def ready(self) -> None:
-        pre_social_login.connect(callback)
