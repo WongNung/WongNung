53abffac46a31ce0f12870cb62a45c07d1d0f444
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-11-16 08:51:22 +0700

    Removes duplicate url entry; replace login/out with POST; closes #56

--- pdtx/urls.py
+++ pdtx/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("wongnung.urls")),
     path("accounts/", include("allauth.urls")),
+    path("", include("wongnung.urls")),
 ]
