c576ee1f725b21a878ffbc6cd09ec6d660757b52
Supakrit65 <supakrit.tan65@hotmail.com>
2022-11-21 21:57:26 +0700

    lower time sleep duration

--- wongnung/tests/test_bookmark_view.py
+++ wongnung/tests/test_bookmark_view.py
@@ -80,7 +80,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.browser.find_element(
             By.CLASS_NAME, bookmark_button_class_name
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
         self.assertIn(
             f"film{self.film_id}-bookmark-yellow-tag",
             self.browser.find_element(
@@ -113,7 +113,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.browser.find_element(
             By.CLASS_NAME, bookmark_button_class_name
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
         self.assertIn(
             f"review{self.review.id}-bookmark-text-tag-yellow",
             self.browser.find_element(
@@ -148,7 +148,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.browser.find_element(
             By.CLASS_NAME, bookmark_button_class_name
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
         self.assertIn(
             f"fandom{self.fandom_name}-bookmarked-text-tag-yellow",
             self.browser.find_element(
@@ -178,7 +178,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.assertIsNotNone(profile_bookmark)
         profile_bookmark[0].click()
         # check film bookmark
-        time.sleep(1)
+        time.sleep(0.5)
         # click Films bookmark radio button
         film_bookmark = list(
             filter(
@@ -189,7 +189,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.assertTrue(film_bookmark)
         self.assertEqual(1, len(film_bookmark))
         film_bookmark[0].click()
-        time.sleep(1)
+        time.sleep(0.5)
         # check if a film exist in profile bookmark
         self.assertTrue(
             self.browser.find_element(By.PARTIAL_LINK_TEXT, self.film.title)
@@ -205,7 +205,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.assertEqual(1, len(review_bookmark))
         # click Reviews bookmark radio button
         review_bookmark[0].click()
-        time.sleep(1)
+        time.sleep(0.5)
         # check if a review exist in profile bookmark
         self.assertTrue(
             self.browser.find_element(By.CLASS_NAME, f"review{self.review.id}")
@@ -221,7 +221,7 @@ class BookmarkViewTest(StaticLiveServerTestCase):
         self.assertEqual(1, len(fandom_bookmark))
         # click Fandoms bookmark radio button
         fandom_bookmark[0].click()
-        time.sleep(1)
+        time.sleep(0.5)
         # check if a fandom exist in profile bookmark
         self.assertTrue(
             self.browser.find_element(By.PARTIAL_LINK_TEXT, self.fandom.name)
