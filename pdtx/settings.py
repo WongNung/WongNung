ab81e97bbab99a7d8a9b6c55be63de501366928f
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-11-22 22:21:35 +0700

    Changes static file storage to another one
    Selenium was failing because of it

--- pdtx/settings.py
+++ pdtx/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
